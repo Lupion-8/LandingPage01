950a02a0add200e022cef39864ec8d42d26e1b44
Lupion-8 <daemon.lupion@gmail.com>
2022-08-06 01:02:10 -0300

    master

---
+++ public/annin.js
@@ -0,0 +1,18 @@
+
+    var socket = io();
+
+
+const contDigital = document.getElementById('um');
+const porqueDigital = document.getElementById('dois');
+const imagem1 = document.getElementById('tres');
+const beneficios = document.querySelectorAll('#cinco');
+console.log(beneficios)
+
+contDigital.classList.add('contDigital');
+porqueDigital.classList.add('porqueDigital');
+imagem1.classList.add('imagem1');
+
+beneficios[0].classList.add('beneficios');
+beneficios[1].classList.add('beneficios');
+beneficios[2].classList.add('beneficios');
+   
