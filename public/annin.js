36562255dfb7d72bdf0a6c3e1a66f6403d47fb20
lupion <daemon.lupion@gmail.com>
2022-08-10 19:39:44 -0300

    master

--- public/annin.js
+++ public/annin.js
@@ -6,7 +6,7 @@ const contDigital = document.getElementById('um');
 const porqueDigital = document.getElementById('dois');
 const imagem1 = document.getElementById('tres');
 const beneficios = document.querySelectorAll('#cinco');
-console.log(beneficios)
+const Quemsomos = document.getElementById('sete');
 
 contDigital.classList.add('contDigital');
 porqueDigital.classList.add('porqueDigital');
@@ -15,4 +15,20 @@ imagem1.classList.add('imagem1');
 beneficios[0].classList.add('beneficios');
 beneficios[1].classList.add('beneficios');
 beneficios[2].classList.add('beneficios');
+
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+
+    console.log(document.documentElement.scrollTop - 50/ document.documentElement.scrollTop) 
+    
+  if (document.documentElement.scrollTop / 700 > 1.0585714285714285 ) {
+    
+    Quemsomos.classList.add('quemSomos');
+
+  } else {
+    Quemsomos.classList.remove('quemSomos');
+  }
+}
    
