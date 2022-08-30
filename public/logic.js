749af78a6450ecf633b81079aeeb819aa5a4525e
lupion <daemon.lupion@gmail.com>
2022-08-30 17:37:24 -0300

    master

--- public/logic.js
+++ public/logic.js
@@ -5,12 +5,12 @@ document.addEventListener("DOMContentLoaded", function() {
     //conteúdo que será compartilhado: Título da página + URL
     var conteudo = encodeURIComponent('Olá, tenho interesse nos serviços');
     //altera a URL do botão
-    document.getElementById("whatsapp-share-btt").href = "https://api.whatsapp.com/send?phone=558738610452&text=Ol%C3%A1%2C%20tenho%20interesse%20nos%20servi%C3%A7os.";
+    document.getElementById("whatsapp-share-btt").href = "https://api.whatsapp.com/send?phone=5574999999999&text=Ol%C3%A1%2C%20tenho%20interesse%20nos%20servi%C3%A7os.";
 }, false);
-
-
-processo.addEventListener('click', ()=>{
-    alert('Inscrição Fechada');
+document.querySelector('button').addEventListener('click', ()=>{
+ alert('Solicitação Enviada!');
+ document.querySelector('.div24 > div'). innerHTML = '<h2>Solicitação Enviada!</h2>';
+ document.querySelector('button').remove();
 });
 
 
